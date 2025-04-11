2ad7af7debc7e8104aa995d0072457f78d295ef6
acasasis-dev <angelito.casasis@gmail.com>
2025-04-11 16:08:57 +0800

    Set each player's name

---
+++ src/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+
+export default function Player({ name, symbol }) {
+	const [ isEditing, setIsEditing ] = useState(false);
+	const [ playerName, setPlayerName ] = useState(name)
+
+	function handleClick() {
+		setIsEditing(!isEditing)
+	}
+
+	function handleChange(e) {
+		setPlayerName(e.target.value);
+	}
+
+	return (
+		<li>
+			<span className="player">
+				{ isEditing? 
+					<input type="text" onChange={ handleChange }/>:
+					<span className="player-name">{ playerName }</span>
+				}
+				<span className="player-symbol">{ symbol }</span>
+				<button onClick={ handleClick }>{ isEditing? "Save": "Edit" }</button>
+			</span>
+		</li>
+	)
+}
