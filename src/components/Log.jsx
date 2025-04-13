a54d43554f40aefeca3d5951735c3f6860f4405c
acasasis-dev <angelito.casasis@gmail.com>
2025-04-13 23:10:27 +0800

    Now showing game turns logs

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,7 +1,14 @@
-export default function Log() {
+export default function Log({ turns }) {
 	return (
 		<ol id="log">
+			{ turns.map(turn => {
+				const { square, player } = turn;
+				const { row, col } = square;
 
+				return (
+					<li key={ `${row}${col}` }>{ player } took square { row }, { col }</li>
+				)
+			}) }
 		</ol>
 	);
 }
